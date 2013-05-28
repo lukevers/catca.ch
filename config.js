1cda3e179b5037b2aaf32bcda7199bc001fece23
Luke Evers <me@lukevers.com>
2013-05-27 22:14:17 -0400

    Started to work on basic structure

---
+++ config.js
@@ -0,0 +1,12 @@
+//  ----------
+//  Cat Cache!
+//  ----------
+//  
+//  config.js
+//  
+//  © 2013 Cat Cache!
+
+var config = require('./config.json');
+var port = config.port;
+
+exports.port = port;
--- server.js
+++ server.js
@@ -12,7 +12,20 @@ var express = require('express');
 var util = require('util');
 
 // Configure
-var app = express().configure(function() {
-    app.use(express.static(__dirname + 'public'));
+var app = express();
+
+app.configure(function() {
+    app.use(express.static(__dirname + '/public'));
     app.use(app.router);
 });
+
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server);
+
+app.get('/', function(req, res) {
+    res.render('index', function(err, html) {
+	
+    });
+});
+
+server.listen(config.port);
