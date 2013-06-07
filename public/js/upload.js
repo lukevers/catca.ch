fac4ef721365c7d6b00a0b1f985203462c82bf49
Luke Evers <me@lukevers.com>
2013-06-07 08:37:03 -0400

    Started working on the upload page

---
+++ public/js/upload.js
@@ -0,0 +1,31 @@
+//  ----------
+//  Cat Cache!
+//  ----------
+//  
+//  common.js
+//
+// © 2013 Cat Cache!
+
+$(document).ready(function(){
+	
+	$('#upload').hide();
+	
+	$('#choose').bind('click', function(event) {
+		$('#filebrowse').click();
+	});
+	
+	$('#filebrowse').bind('change', function(event) {
+		$('.files').html('');
+		var files = $.map($('#filebrowse').prop('files'), function(val) { return val.name; });
+		$('.files').prepend('<br/>');
+		for (x in files) {
+			$('.files').append('<div class="alert">'+files[x]+'</div>');
+		}
+		$('#upload').fadeIn();
+	});
+	
+	$('#upload').bind('click', function(event) {
+		
+	});
+	
+});
