8abaa772ce572c313d510366e9e4f33fc806ac7d
Luke Evers <me@lukevers.com>
2013-06-06 20:34:39 -0400

    Fixed redirect errors.
    I was redirecting and then redirecting.
    That was extremely stupid, and now I'm
    just redirecting once.

--- get.js
+++ get.js
@@ -16,31 +16,32 @@ module.exports = function(app, db) {
 	
 	// Sign In
 	app.get('/signin', function(req, res) {
-		if (typeof req.session.user == 'undefined') res.render('signin', { 'loggedin': 'false' });
-		else res.render('signin', { 'loggedin': 'true', 'user': req.session.user });
+		if (typeof req.session.user == 'undefined') res.render('signin');
+		else res.redirect('/account');
 	});
 	
 	// Sign Up
 	app.get('/signup', function(req, res) {
-		if (typeof req.session.user == 'undefined') res.render('signup', { 'loggedin': 'false' });
-		else res.render('signup', { 'loggedin': 'true', 'user': req.session.user });
+		if (typeof req.session.user == 'undefined') res.render('signup');
+		else res.redirect('/account');
 	});
 	
 	// Recover Password
 	app.get('/recover', function(req, res) {
-		if (typeof req.session.user == 'undefined') res.render('recover', { 'loggedin': 'false' });
-		else res.render('recover', { 'loggedin': 'true', 'user': req.session.user });
+		if (typeof req.session.user == 'undefined') res.render('recover');
+		else res.redirect('/account');
 	});
 	
 	// Account
 	app.get('/account', function(req, res) {
-		if (typeof req.session.user == 'undefined') res.render('account', { 'loggedin': 'false' });
-		else res.render('account', { 'loggedin': 'true', 'user': req.session.user });
+		if (typeof req.session.user == 'undefined') res.redirect('/signin#not_logged_in');
+		else res.render('account', { 'user': req.session.user });
 	});
 	
 	// Upload
 	app.get('/upload', function(req, res) {
-		
+		if (typeof req.session.user == 'undefined') res.redirect('/signin#not_logged_in');
+		else res.render('upload', { 'user': req.session.user });
 	});
 	
 	// Liked By
